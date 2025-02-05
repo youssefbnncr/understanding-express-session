6491185c46db7c77d30572ea4adbde701beafbbe
Youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-05 19:38:20 +0100

    setup the views engine and routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine","ejs");
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-  res.send("Hello from the root");
+  res.send('index');
 })
 
+app.get('/users/register',(req,res)=>{
+  res.render('register')
+})
+
+app.get('/users/login',(req,res)=>{
+  res.render('login')
+})
+
+app.get('/users/dashboard',(req,res)=>{
+  res.render('dashboard',{user: "Youssef"})
+})
 app.listen(PORT,()=>{
   console.log("Server Running");
 })
